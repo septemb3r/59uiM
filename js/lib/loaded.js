84eded8ea868b3203403347a1c79d0696026453e
septemb3r <thelostwind93@gmail.com>
2016-09-05 09:03:57 +0700

    chess

---
+++ js/lib/loaded.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+console.log("%cBớt hack đi tao báo công an bây giờ", "font-size: 3.5em;font-family: Lato;color: white;background-color:black");
